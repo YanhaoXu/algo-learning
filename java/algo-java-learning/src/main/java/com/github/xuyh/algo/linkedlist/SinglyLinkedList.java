c6fea8db9a9a8024d94ca7fafb4204b74a693397
yanhao.xu <yanhao.xu@outlook.com>
2022-01-03 22:02:10 +0800

    feature(SinglyLinkedList): add

--- java/algo-java-learning/src/main/java/com/github/xuyh/algo/linkedlist/SinglyLinkedList.java
+++ java/algo-java-learning/src/main/java/com/github/xuyh/algo/linkedlist/SinglyLinkedList.java
@@ -21,6 +21,29 @@ public class SinglyLinkedList {
     return p;
   }
 
+  /**
+   * 带头节点的链表反转 等价于从第二个节点开始从新头插入建立链表
+   *
+   * @param p
+   * @return
+   */
+  public Node inverseLinkedListHead(Node p) {
+    Node Head = new Node(0, null);
+
+    Head.next = p;
+    Node Cur = p.next;
+    Node next = null;
+
+    while (Cur != null) {
+      next = Cur.next;
+      Cur.next = Head.next;
+      Head.next = Cur;
+      Cur = next;
+    }
+
+    return Head;
+  }
+
   public class Node {
     private int data;
     private Node next;
