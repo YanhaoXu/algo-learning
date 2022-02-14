58fcd38df7e755ab12fe449f040b87c1ad17b3e7
yanhao.xu <yanhao.xu@outlook.com>
2022-02-14 23:34:42 +0800

    变更

--- java/algo-java-learning/src/main/java/com/github/xuyh/algo/sorts/QuickSort.java
+++ java/algo-java-learning/src/main/java/com/github/xuyh/algo/sorts/QuickSort.java
@@ -1,3 +1,14 @@
 package com.github.xuyh.algo.sorts;
 
-public class QuickSort {}
+public class QuickSort {
+
+  public static void quickSort(int[] data) {
+
+
+
+  }
+
+  public static void main(String[] args) {
+    //
+  }
+}
